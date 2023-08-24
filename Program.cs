4fcaaeb6f3516b8810b634b850cb4dd772af1254
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-08-23 21:28:42 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿string sourcePath = "file.csv";
+string targetPath = "summary.csv";
+string targetFolder = "out";
+string outPath = Path.Combine(targetFolder, targetPath);
+
+try
+{
+  if (!Directory.Exists(targetFolder))
+  {
+    Directory.CreateDirectory(targetFolder);
+  }
+  else
+  {
+    File.Create(outPath).Close();
+  }
+
+  string[] lines = File.ReadAllLines(sourcePath);
+
+  using (StreamWriter sw = File.AppendText(outPath))
+  {
+    foreach(string line in lines)
+    {
+      var splitLine = line.Split(",");
+      string product = splitLine[0];
+      double price = double.Parse(splitLine[1]);
+      int quantity = int.Parse(splitLine[2]);
+      double total = price * quantity;
+
+      sw.WriteLine(@$"{product},{total:F2}");
+    }
+  }
+}
+catch (IOException e)
+{
+  Console.WriteLine(e.Message);
+}
